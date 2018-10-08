c7072291e7e6cb0ad0067d1ef670aef0ea9e68f8
mochilefty <mochi.lefty@gmail.com>
2018-10-08 16:11:28 +0900

    Updata search.js

--- app/assets/javascripts/modal_search-form.js
+++ app/assets/javascripts/modal_search-form.js
@@ -1,17 +1,3 @@
-$(document).on("turbolinks:load", function(){
-
-  var html = `
-    <div class="box__search-form">
-      test
-    </div>`
-
-  $("#btn_search-form").click(function(e){
-    e.preventDefault();
-    $("body").append(html);
-
-    $("#filter").click(function(){
-      $(".box__serch-form").remove();
-    });
-  });
+$(function(){
 
 });
