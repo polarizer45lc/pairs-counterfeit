5a4623ef54816d5998d1410c0593a9febaa52332
polarizer45lc <m1873.45lc@gmail.com>
2018-10-08 19:02:34 +0900

    Bootstrap

--- app/assets/javascripts/tweet_rule.js
+++ app/assets/javascripts/tweet_rule.js
@@ -1,13 +1,16 @@
 $(function(){
 
   var html = `
-              <div class="tweet_rule" style="width:100%;height:900px;position:absolute;left:0;top:50px;z-index:1000;">
-                <div class="tweet_rule_inner" style="width:600px;height:900px;background-color:#FFFFFF;position:relative;top:20px;margin: 0 auto;">
-                </div>
-              </div>`
+  <div class="tweet_rule" style="width:100%;height:900px;position:absolute;left:0;top:50px;z-index:1000;">
+    <div class="tweet_rule_inner" style="width:600px;height:900px;background-color:#FFFFFF;position:relative;top:20px;margin: 0 auto;">
+      <div class="model_window_inner" style="width:100%;heigt:100%;padding:15px;">
+        <p class="model_sub_title">hello</p>
+      </div>
+    </div>
+  </div>`
 
   $('#tweet_rule').on('click', function(e){
-    $("body").append(html).hide().fadeIn('slow');
+    $("body").append(html).hide().fadeIn(200);
     // $("tweet_rule").fadeIn(9000);
     // e.preventDefault();
     // console.log("ok")
