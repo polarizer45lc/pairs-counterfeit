03e09b4d647fa5187abf2f104d4c756ee8c4d048
KazumaUede <katsuodashi794@gmail.com>
2018-10-09 14:58:13 +0900

    Fix devise_parameter_sanitizer.permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :tell, :sex, :birthday, :hearts])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :tell, :sex, :birthday])
   end
 end
