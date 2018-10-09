f12969a70f59dfa3a9f604dddb7eb4288906bd6b
koyamada314 <shinbones.s9@gmail.com>
2018-10-09 18:04:43 +0900

    Fix-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers:{
+  	confirmations: 'users/confirmations',
+  	omniauth: 'users/omniauth',
+  	passwords: 'users/passwords',
+  	registrations: 'users/registrations',
+  	sessions: 'users/sessions',
+  	unlocks: 'users/unlocks'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 
