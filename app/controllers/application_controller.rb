2d81ad25016fef42772409e4095003fbc2abdc03
fumiya0312 <good.job0312@gmail.com>
2018-10-10 18:36:59 +0900

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :sex, :email, :birthday, :country])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :sex, :email, :birthday])
 
   end
 end
