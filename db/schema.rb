142ab8e8f734e0ce4d8e3fcc981969c8d12d2747
mochilefty <mochi.lefty@gmail.com>
2018-10-11 14:13:33 +0900

    Update application_controller
    before_actionをコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -128,9 +128,10 @@ ActiveRecord::Schema.define(version: 20181009074241) do
 
   create_table "regions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
-    t.integer  "overseas"
+    t.integer  "overseas",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name", "overseas"], name: "index_regions_on_name_and_overseas", unique: true, using: :btree
   end
 
   create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -144,6 +145,7 @@ ActiveRecord::Schema.define(version: 20181009074241) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.integer  "tell"
     t.string   "encrypted_password",     default: "", null: false
