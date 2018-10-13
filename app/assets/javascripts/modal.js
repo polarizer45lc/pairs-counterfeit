70135aaeadd337b1d11074371fa06c9d804d908d
polarizer45lc <m1873.45lc@gmail.com>
2018-10-13 17:42:24 +0900

    wip_pulldown

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -31,3 +31,25 @@ $(document).on('turbolinks:load', function(){
     trigger: 'click'
   });
 });
+
+
+
+
+
+
+
+
+function clickBtn_profile(){
+
+  const color1 = document.form1.color1;
+
+  // 値(数値)を取得
+  const num = color1.selectedIndex;
+  //const num = document.form1.color1.selectedIndex;
+
+  // 値(数値)から値(value値)を取得
+  const str = color1.options[num].value;
+  //const str = document.form1.color1.options[num].value;
+
+  document.getElementById("span1").textContent = str;
+}
