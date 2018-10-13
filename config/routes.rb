bdb46262f4b1a72065eadf4a8555407654de1b20
koyamada314 <shinbones.s9@gmail.com>
2018-10-13 18:28:26 +0900

    middlecommit

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 
-  resources :users
+  resources :users do
+    member do
+    post :user_edit
+    end
+  end
 end
