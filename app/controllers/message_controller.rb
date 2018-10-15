4194e165b73d1124d140f4d2a1555f845d1d0b7d
KazumaUede <katsuodashi794@gmail.com>
2018-10-15 17:53:51 +0900

    delete messeage commentout,test,and coffee

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -3,21 +3,11 @@ class MessageController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    # respond_to do |format|
-    #   format.html
-    #   # まだ画面上に更新されてないメッセージがあれば取得
-    #   # where句の?は隣のparams[:id]のこと。jsonから画面上のメッセージIdを取得して比較する。
-    #   format.json { @new_messages = @messages.where('id > ?', params[:id]) }
-    # end
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      # respond_to do |format|
-      #   format.html { redirect_to group_messages_path(@group) }
-      #   format.json
-      # end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください"
