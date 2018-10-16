cd2d4a271dc6ad9d23523a8019f2b26293c45e6c
KazumaUede <katsuodashi794@gmail.com>
2018-10-16 16:59:15 +0900

    Fix model group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,7 +7,7 @@ class Group < ApplicationRecord
 
   def show_last_message
     if (last_message = messages.last).present?
-      last_message.text? ? last_message.text : '画像が投稿されています'
+      last_message.text.presence || '画像が投稿されています'
     else
       'メッセージ交換をしていません。'
     end
