5eb113e3815ebadcdb86acd5cf5812c942c39482
KazumaUede <katsuodashi794@gmail.com>
2018-10-16 15:56:32 +0900

    Add message controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
-
-  resources :groups, only: [:index, :new, :create] do
-    resources :messages, only: [:index, :create]
-  end
-
   resources :relationships, only: [:create, :destroy]
   resources :users do
+    resources :groups, only: [:index, :new, :create] do
+      resources :messages, only: [:index, :create]
+    end
+  end
+  resources :users do
     member do
-     get :followings, :followedes, :matches
-     post :user_edit
+      get :followings, :followedes
+      post :user_edit
     end
   end
 end
