7b09a8c99b8e15572e6a5e684caed930418790a7
fumiya0312 <good.job0312@gmail.com>
2018-10-17 13:06:13 +0900

    Devise add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def index   #自分の性別以外を取得する状態です。条件弄ったらこのコメントアウト消してください。
     current_user.sex == 1 ? @users = User.where(sex: 0).page(params[:page]).per(16).order("created_at DESC"): @users = User.where(sex: 1).page(params[:page]).per(16).order("created_at DESC")
-    @user = current_user.id
   end
 
   def show
