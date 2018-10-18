73265706ee70974b6dff34ea0e22b9b05f6bbecb
fumiya0312 <good.job0312@gmail.com>
2018-10-18 18:21:48 +0900

    Helper.fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -16,21 +16,21 @@ module UsersHelper
     end
   end
 
-  def default_avatar
-    if self.male?
-      "no-image__woman.png"
-    else
-      "no-image__man.png"
-    end
-  end
+  # def default_avatar
+  #   if self.male?
+  #     "no-image__woman.png"
+  #   else
+  #     "no-image__man.png"
+  #   end
+  # end
 
-  def main_avatar
-    if self.avatar.present?
-      self.avatar
-    else
-      self.default_avatar
-    end
-  end
+  # def main_avatar
+  #   if self.avatar.present?
+  #     self.avatar
+  #   else
+  #     self.default_avatar
+  #   end
+  # end
 
 end
 
