f51aaf7703d41b692802770c5e063f4ab07c158c
fumiya0312 <good.job0312@gmail.com>
2018-10-20 23:31:12 +0900

    check_mark JavaScript fix

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,31 +1,52 @@
 $(document).on('turbolinks:load', function(){
-  function buildHTML(){
+    var submit_button = $('#pairs_submit');
+    submit_button.prop('disabled', true);
 
-    var html = `
-                <img class="check_mark_on" src="/assets/mark-on.png" alt="mark-on">`
-
-    return html;
-  }
-
-  function returnHTML(){
-
-    var html = `
-                <img class="check_mark_off" src="/assets/mark-off.png" alt="mark-off">`
-
-    return html;
-  }
-  $(document).on('click', ".check_mark_off", function(e)
-    {
-      $(".check_mark_off").remove();
-        var html = buildHTML(e);
-        $(".mark_box").append(html)
-        e.preventDefault();
-    });
-  $(document).on('click', ".check_mark_on", function(e)
-    {
-      $(".check_mark_on").remove();
-        var html = returnHTML(e);
-        $(".mark_box").append(html)
-        e.preventDefault();
-    });
+  $("#check_mark").on("click", function(){
+  if ($(this).hasClass("check_on")){
+        $(this).removeClass("check_on");
+        submit_button.removeClass("submit_on").prop('disabled', true);
+  } else {
+        $(this).addClass("check_on");
+        submit_button.addClass("submit_on").prop('disabled', false);
+         }
+  });
 });
+
+
+  // function buildHTML(){
+
+  //   var html = `
+  //               <img class="check_mark_on" src="/assets/mark-on.png" alt="mark-on">`
+
+  //   return html;
+  // }
+
+  // function returnHTML(){
+
+  //   var html = `
+  //               <img class="check_mark_off" src="/assets/mark-off.png" alt="mark-off">`
+
+  //   return html;
+  // }
+  // $(document).on('click', ".check_mark_off", function(e)
+  //   {
+  //     $(".check_mark_off").remove();
+  //       var html = buildHTML(e);
+  //       $(".mark_box").append(html)
+  //       e.preventDefault();
+  //   });
+  // $(document).on('click', ".check_mark_on", function(e)
+  //   {
+  //     $(".check_mark_on").remove();
+  //       var html = returnHTML(e);
+  //       $(".mark_box").append(html)
+  //       e.preventDefault();
+  //   });
+
+
+
+
+
+
+
