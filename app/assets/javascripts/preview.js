d981c2b5586f247536ad6ab5ef3007744ed326a9
fumiya0312 <good.job0312@gmail.com>
2018-10-22 14:22:05 +0900

    JavaScript button

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,9 +1,8 @@
-$(function(){
-  $(document).on('turbolinks:load', function(){
+$(document).on('turbolinks:load', function(){
+  $(function(){
     $('#main_avatar_uploader').change(function(e){
       var file = e.target.files[0];
       var reader = new FileReader();
-        $('img').removeClass('none');
       if(file.type.indexOf("image") < 0){
         alert("画像ファイルを指定して下さい。");
         return false;
@@ -13,6 +12,7 @@ $(function(){
         return function(e){
           $("#preview_avatar").attr("src", e.target.result);
           $("#preview_avatar").attr("title", file.name)
+          $('img').removeClass('none');
         };
       })(file);
       reader.readAsDataURL(file);
