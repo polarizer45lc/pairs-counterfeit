af7dd8231468b32ec87f3550748029f6e3f0e337
polarizer45lc <m1873.45lc@gmail.com>
2018-10-22 15:48:13 +0900

    repair_preview_avatar

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,21 +1,19 @@
-$(document).on('turbolinks:load', function(){
-  $(function(){
-    $('#main_avatar_uploader').change(function(e){
-      var file = e.target.files[0];
-      var reader = new FileReader();
-      if(file.type.indexOf("image") < 0){
-        alert("画像ファイルを指定して下さい。");
-        return false;
-      }
-      reader.onload = (function(file){
-        $(".hidden").remove();
-        return function(e){
-          $("#preview_avatar").attr("src", e.target.result);
-          $("#preview_avatar").attr("title", file.name)
-          $('img').removeClass('none');
-        };
-      })(file);
-      reader.readAsDataURL(file);
-    });
+$(function(){
+  $('#main_avatar_uploader').change(function(e){
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+    reader.onload = (function(file){
+      $(".hidden").css('background-image', 'none');
+      return function(e){
+        $("#preview_avatar").attr("src", e.target.result);
+        $("#preview_avatar").attr("title", file.name);
+        $('img').removeClass('none');
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
 });
